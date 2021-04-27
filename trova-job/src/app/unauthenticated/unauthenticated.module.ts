be2d111cc303c5d3a7be56366a043a2659a10d53
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-04-27 12:51:06 +0200

    generate the initial main components for the first module

--- trova-job/src/app/unauthenticated/unauthenticated.module.ts
+++ trova-job/src/app/unauthenticated/unauthenticated.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UnauthenticatedRoutingModule } from './unauthenticated-routing.module';
+import { ElementsWrapperComponent } from './elements-wrapper/elements-wrapper.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { SignInComponent } from './wrapper/sign-in/sign-in.component';
+import { SignUpComponent } from './wrapper/sign-up/sign-up.component';
+import { WelcomePageComponent } from './wrapper/welcome-page/welcome-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ElementsWrapperComponent, WrapperComponent, SignInComponent, SignUpComponent, WelcomePageComponent],
   imports: [
     CommonModule,
     UnauthenticatedRoutingModule
