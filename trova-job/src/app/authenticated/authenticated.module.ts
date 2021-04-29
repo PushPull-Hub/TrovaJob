df8559f82d640c46ec5577e1fb58a80bebe7d2e2
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-04-29 17:47:00 +0200

    import the needed angular material modules

--- trova-job/src/app/authenticated/authenticated.module.ts
+++ trova-job/src/app/authenticated/authenticated.module.ts
@@ -1,14 +1,49 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { HomeComponent } from './wrapper/home/home.component';
+import { NavbarComponent } from './wrapper/navbar/navbar.component';
+import { ProfileComponent } from './wrapper/profile/profile.component';
+
+import { SharedModule } from '../shared/shared.module';
 import { AuthenticatedRoutingModule } from './authenticated-routing.module';
 
+// Angular Material
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+const AngularMaterialModules = [
+  MatCardModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatIconModule,
+  MatNativeDateModule,
+  MatProgressSpinnerModule,
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WrapperComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProfileComponent,
+  ],
   imports: [
     CommonModule,
-    AuthenticatedRoutingModule
-  ]
+    AuthenticatedRoutingModule,
+    SharedModule,
+    ...AngularMaterialModules,
+  ],
 })
-export class AuthenticatedModule { }
+export class AuthenticatedModule {}
