aa558fcbb49095ab7165d78a5362a671add21efe
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-04-29 17:43:56 +0200

    setup the routes for the authenticated module

--- trova-job/src/app/authenticated/authenticated-routing.module.ts
+++ trova-job/src/app/authenticated/authenticated-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './wrapper/home/home.component';
+import { ProfileComponent } from './wrapper/profile/profile.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'find-jobs', pathMatch: 'full' },
+  {
+    path: '',
+    component: WrapperComponent,
+    children: [
+      { path: 'find-jobs', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthenticatedRoutingModule { }
+export class AuthenticatedRoutingModule {}
