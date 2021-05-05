88c1bbffa0c68d65db600156b23f1c7e07af72ef
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-05 16:43:32 +0200

    add a setter to the model

--- trova-job/src/app/models/user.model.ts
+++ trova-job/src/app/models/user.model.ts
@@ -7,10 +7,33 @@ export class User extends Object {
   adress: string;
   birthday: Birthday;
   phoneNumber: number;
+
+  public setUser(
+    id: string,
+    email: string,
+    password: string,
+    username: string,
+    role: Role,
+    adress: string,
+    birthday: Birthday,
+    phoneNumber: number
+  ) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.username = username;
+    this.role = role;
+    this.adress = adress;
+    this.birthday = birthday;
+    this.phoneNumber = phoneNumber;
+  }
 }
 
-export class Birthday {
-  constructor(public day: number, public month: number, public year: number) {}
+export class Birthday extends Object {
+  public day: number;
+  public month: number;
+  public year: number;
+  // constructor(public day: number, public month: number, public year: number) {}
 }
 
 type Role = 'admin' | 'user';
