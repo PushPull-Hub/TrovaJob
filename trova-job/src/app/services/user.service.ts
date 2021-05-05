2b8d6a084d1b9251a2d6bcf8cdaa85060b223824
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-05 17:58:43 +0200

    create the initial methods to check the user privileges

--- trova-job/src/app/services/user.service.ts
+++ trova-job/src/app/services/user.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor() {}
+
+  canCreate(user: User): boolean {
+    const allowed = ['admin', 'user'];
+    return this.checkAuthorization(user, allowed);
+  }
+
+  canRead(user: User): boolean {
+    const allowed = ['admin', 'user'];
+    return this.checkAuthorization(user, allowed);
+  }
+
+  canEdit(user: User): boolean {
+    const allowed = ['admin'];
+    return this.checkAuthorization(user, allowed);
+  }
+
+  canDelete(user: User): boolean {
+    const allowed = ['admin'];
+    return this.checkAuthorization(user, allowed);
+  }
+
+  private checkAuthorization(user: User, allowedRoles: string[]): boolean {
+    if (!user) return false;
+    for (const role of allowedRoles) {
+      if (user.role[role]) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
