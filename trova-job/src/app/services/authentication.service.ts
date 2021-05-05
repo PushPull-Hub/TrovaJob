6d6be5f5e92d4e9ec5ad1cd33fe9e8541d1fd6aa
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-05 17:56:25 +0200

    fix the sign-up method

--- trova-job/src/app/services/authentication.service.ts
+++ trova-job/src/app/services/authentication.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { AngularFireAuth } from '@angular/fire/auth';
-import {
-  AngularFirestore,
-  AngularFirestoreDocument,
-} from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -27,77 +24,63 @@ export class AuthenticationService {
     return this.angularFireAuth.signInWithEmailAndPassword(email, password);
   }
 
-  signUp(user: User) {
-    this.signIn(user.email, user.password)
-      .then((data) => {
-        if (data && data.user.uid) {
-          return this.angularFireAuth.createUserWithEmailAndPassword(
-            user.email,
-            user.password
-          );
-        }
-      })
-      .catch((error) => console.log(error));
-  }
-
-  createUserOnFireStore(user: User): Promise<void> {
-    const userRef: AngularFirestoreDocument<any> = this.angularFirestore.doc(
-      `users/${user.id}`
+  async signUp(user: User): Promise<boolean> {
+    const firbaseResponse = await this.angularFireAuth.createUserWithEmailAndPassword(
+      user.email,
+      user.password
     );
-    const data = this.createUserObjectFromUserCustomClass(user);
-    return userRef.set(data, { merge: true });
-  }
 
-  createUserObjectFromUserCustomClass(user: User) {
-    return {
-      // id: user.id,
-      // email: user.email,
-      // password: user.password,
-      username: user.username,
-      role: user.role,
-      adress: user.adress,
-      birthday: user.birthday,
-      phoneNumber: user.phoneNumber,
-    };
+    try {
+      return firbaseResponse && firbaseResponse.user.uid
+        ? this.createUserOnFireStore(user, firbaseResponse.user.uid)
+        : of(false).toPromise();
+    } catch (error) {
+      of(false).toPromise();
+    }
   }
 
   async logOut(): Promise<void> {
     await this.angularFireAuth
       .signOut()
-      .then((data) => {
+      .then(() => {
         this.loggedInUser.next(null);
         this.router.navigateByUrl('authentication/sign-in');
       })
       .catch((error) => console.log(error));
   }
 
-  canCreate(user: User): boolean {
-    const allowed = ['admin', 'user'];
-    return this.checkAuthorization(user, allowed);
-  }
-
-  canRead(user: User): boolean {
-    const allowed = ['admin', 'user'];
-    return this.checkAuthorization(user, allowed);
-  }
-
-  canEdit(user: User): boolean {
-    const allowed = ['admin'];
-    return this.checkAuthorization(user, allowed);
-  }
-
-  canDelete(user: User): boolean {
-    const allowed = ['admin'];
-    return this.checkAuthorization(user, allowed);
-  }
-
-  private checkAuthorization(user: User, allowedRoles: string[]): boolean {
-    if (!user) return false;
-    for (const role of allowedRoles) {
-      if (user.role[role]) {
-        return true;
-      }
+  private async createUserOnFireStore(
+    user: User,
+    firebaseUserId: any
+  ): Promise<boolean> {
+    try {
+      const data = await this.createUserObjectFromUserCustomClass(
+        user,
+        firebaseUserId
+      );
+      this.angularFirestore
+        .doc(`users/${firebaseUserId}`)
+        .set(data, { merge: true });
+      this.loggedInUser.next(user);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-    return false;
+  }
+
+  private createUserObjectFromUserCustomClass(
+    user: User,
+    firebaseUserId: any
+  ): Promise<any> {
+    return of({
+      id: firebaseUserId,
+      email: user.email,
+      username: user.username,
+      role: user.role,
+      adress: user.adress,
+      birthday: user.birthday,
+      phoneNumber: user.phoneNumber,
+    }).toPromise();
   }
 }
