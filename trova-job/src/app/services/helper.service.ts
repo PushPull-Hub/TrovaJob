6afaa4f36153011025ffb0db9f04e416417dec21
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-07 13:06:11 +0200

    split the secondary methods to an extrnal helper service

---
+++ trova-job/src/app/services/helper.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { User } from '../models/user.model';
+
+type applicationPaths =
+  | 'home'
+  | 'authentication/sign-in'
+  | 'authentication/sign-up'
+  | 'profile';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapperFunctionsService {
+  constructor(private router: Router) {}
+
+  createUserObjectFromUserCustomClass(
+    user: User,
+    firebaseUserId: any
+  ): Promise<any> {
+    return of({
+      id: firebaseUserId,
+      email: user.email,
+      username: user.username,
+      role: user.role,
+      adress: user.adress,
+      birthday: user.birthday,
+      phoneNumber: user.phoneNumber,
+    }).toPromise();
+  }
+
+  convertFirebaseObjectToUserObject(firebaseObject: {
+    [key: string]: any;
+  }): Promise<User> {
+    const user = new User();
+    user.adress = firebaseObject.adress ?? null;
+    user.birthday = firebaseObject.birthday ?? null;
+    user.email = firebaseObject.email ?? null;
+    user.password = firebaseObject.password ?? null;
+    user.phoneNumber = firebaseObject.phoneNumber ?? null;
+    user.role = firebaseObject.role ?? null;
+    user.id = firebaseObject.id ?? null;
+    user.username = firebaseObject.username ?? null;
+    return of(user).toPromise();
+  }
+
+  redirectTo(path: applicationPaths) {
+    this.router.navigateByUrl(path);
+  }
+}
