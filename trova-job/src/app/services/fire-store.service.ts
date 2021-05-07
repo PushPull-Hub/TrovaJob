fe6e947de1479e7fdf5852b3792d889223301013
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-07 13:07:06 +0200

    generate a service to perform actions on the firestore

---
+++ trova-job/src/app/services/fire-store.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from '../models/user.model';
+import { MapperFunctionsService } from './helper.service';
+
+@Injectable({ providedIn: 'root' })
+export class FireStoreCustomService {
+  constructor(
+    private angularFirestore: AngularFirestore,
+    private helperFunctionsService: MapperFunctionsService
+  ) {}
+
+  async createUserOnFireStore(
+    user: User,
+    firebaseUserId: any
+  ): Promise<boolean> {
+    try {
+      const data = await this.helperFunctionsService.createUserObjectFromUserCustomClass(
+        user,
+        firebaseUserId
+      );
+      this.angularFirestore
+        .doc(`users/${firebaseUserId}`)
+        .set(data, { merge: true });
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  getLoggedInUserDataFromFireStore(userId) {
+    this.angularFirestore
+      .doc(`users/${userId}`)
+      .valueChanges()
+      .subscribe(async (data) => {
+        return await this.helperFunctionsService.convertFirebaseObjectToUserObject(
+          data
+        );
+      });
+  }
+}
