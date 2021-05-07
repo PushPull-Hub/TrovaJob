8f610f2c3437b263945eeb242cc6347f762ce7dc
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-07 13:08:58 +0200

    move all the logic to the services and clean the components code

--- trova-job/src/app/services/authentication.service.ts
+++ trova-job/src/app/services/authentication.service.ts
@@ -4,39 +4,54 @@ import { BehaviorSubject, of } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { MapperFunctionsService } from './helper.service';
+import { CustomErrorObject } from '../models/error.model';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
   loggedInUser: BehaviorSubject<User> = new BehaviorSubject<User>(null);
-  isSomeFormLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
-    null
-  );
+  isFormLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
 
   constructor(
     private angularFireAuth: AngularFireAuth,
     private angularFirestore: AngularFirestore,
+    private helperFunctionsService: MapperFunctionsService,
+    private errorService: ErrorService,
     private router: Router
   ) {}
 
-  signIn(email: string, password: string): Promise<any> {
-    return this.angularFireAuth.signInWithEmailAndPassword(email, password);
+  signIn(email: string, password: string) {
+    this.angularFireAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((res) => {
+        this.getLoggedInUserDataFromFireStore(res.user.uid);
+      })
+      .catch((err) => {
+        const error = new CustomErrorObject(true, err.message, 400);
+        this.errorService.errorOnSignIn.next(error);
+      });
   }
 
-  async signUp(user: User): Promise<boolean> {
-    const firbaseResponse = await this.angularFireAuth.createUserWithEmailAndPassword(
-      user.email,
-      user.password
-    );
-
-    try {
-      return firbaseResponse && firbaseResponse.user.uid
-        ? this.createUserOnFireStore(user, firbaseResponse.user.uid)
-        : of(false).toPromise();
-    } catch (error) {
-      of(false).toPromise();
-    }
+  signUp(user: User) {
+    new Promise((resolve) => {
+      this.angularFireAuth
+        .createUserWithEmailAndPassword(user.email, user.password)
+        .then(async (fireAuthResponse) => {
+          if (fireAuthResponse.user.uid) {
+            const savingUserOnFireStore: boolean = await this.createUserOnFireStore(
+              user,
+              fireAuthResponse.user.uid
+            );
+            resolve(savingUserOnFireStore);
+          }
+        })
+        .then((finalResult: any) => {
+          finalResult ? this.helperFunctionsService.redirectTo('home') : null;
+        });
+    });
   }
 
   async logOut(): Promise<void> {
@@ -44,7 +59,7 @@ export class AuthenticationService {
       .signOut()
       .then(() => {
         this.loggedInUser.next(null);
-        this.router.navigateByUrl('authentication/sign-in');
+        this.helperFunctionsService.redirectTo('authentication/sign-in');
       })
       .catch((error) => console.log(error));
   }
@@ -54,7 +69,7 @@ export class AuthenticationService {
     firebaseUserId: any
   ): Promise<boolean> {
     try {
-      const data = await this.createUserObjectFromUserCustomClass(
+      const data = await this.helperFunctionsService.createUserObjectFromUserCustomClass(
         user,
         firebaseUserId
       );
@@ -63,24 +78,32 @@ export class AuthenticationService {
         .set(data, { merge: true });
       this.loggedInUser.next(user);
       return true;
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      const error = new CustomErrorObject(true, err.message, 400);
+      this.errorService.errorOnSignUp.next(error);
       return false;
     }
   }
 
-  private createUserObjectFromUserCustomClass(
-    user: User,
-    firebaseUserId: any
-  ): Promise<any> {
-    return of({
-      id: firebaseUserId,
-      email: user.email,
-      username: user.username,
-      role: user.role,
-      adress: user.adress,
-      birthday: user.birthday,
-      phoneNumber: user.phoneNumber,
-    }).toPromise();
+  private getLoggedInUserDataFromFireStore(userId) {
+    this.angularFirestore
+      .doc(`users/${userId}`)
+      .valueChanges()
+      .subscribe(async (data) => {
+        try {
+          const user = await this.helperFunctionsService.convertFirebaseObjectToUserObject(
+            data
+          );
+          this.loggedInUser.next(user);
+          this.helperFunctionsService.redirectTo('home');
+        } catch (error) {
+          const customError = new CustomErrorObject(
+            true,
+            'an error occured during recovery of user data from database, try again',
+            401
+          );
+          this.errorService.errorOnSignIn.next(customError);
+        }
+      });
   }
 }
