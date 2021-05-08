b243b25874c478619bd7876ea1a773f6a617d3e2
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-08 23:45:02 +0200

    create a custom errors enum

--- trova-job/src/app/services/error.service.ts
+++ trova-job/src/app/services/error.service.ts
@@ -11,5 +11,17 @@ export class ErrorService {
     null
   );
 
+  errorOnSignOut: BehaviorSubject<CustomErrorObject> = new BehaviorSubject<CustomErrorObject>(
+    null
+  );
+
   constructor() {}
+}
+
+export enum FireBaseErrors {
+  onFireAuthSignUp = 'an error occured during creating user on Firebase Authentication system',
+  onFireAuthSignIn = 'an error occured during signing user on Firebase Authentication system',
+  onFireAuthSignOut = 'an error occured during signing out from the Firebase Authentication system',
+  onFireStoreSetUser = 'an error occured during procuder of store user data ',
+  onFireStoreRetrieveUser = ' an error occured during recovery of user data from the database',
 }
