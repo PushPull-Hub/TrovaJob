f34244852095d88f51bf10f68cbcf67aa41534c0
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-10 17:47:07 +0200

    clean code and import icons

--- trova-job/src/app/components/dynamic-components/card/card.component.ts
+++ trova-job/src/app/components/dynamic-components/card/card.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faSearch,
+  faUsersCog,
+  faIdCard,
+  faBriefcase,
+  faUserShield,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  cardTitle: string;
+  searchIcon;
+  UsersSettingsIcon;
+  profileSettingsIcon;
+  jobsSettingsIcon;
+  adminIcon;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.searchIcon = faSearch;
+    this.UsersSettingsIcon = faUsersCog;
+    this.profileSettingsIcon = faIdCard;
+    this.jobsSettingsIcon = faBriefcase;
+    this.adminIcon = faUserShield;
+    this.cardTitle = 'Look For New Offerts';
   }
 
+  onCardClick() {
+    console.log('card clicked');
+  }
 }
