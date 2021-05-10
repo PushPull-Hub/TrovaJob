72bce1e6ca9cf064273621d16a109fa29c03fdf9
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-10 11:52:23 +0200

    add font awesome library to use more elegant icons

--- trova-job/src/app/app.module.ts
+++ trova-job/src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
-import { environment } from '../environments/environment';
 import { CreateComponentDirective } from './directives/create-component.directive';
 
 @NgModule({
