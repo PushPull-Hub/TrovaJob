0b54abb678a933620b21a33091d4a90d327be6c4
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-11 13:05:46 +0200

    set a new model foe user abilities

---
+++ trova-job/src/app/models/abilities.model.ts
@@ -0,0 +1,15 @@
+export class UserAbilities extends Array {
+  canReadUsers: boolean;
+  canCreateUsers: boolean;
+  canUpdateUsers: boolean;
+  canDeleteteUsers: boolean;
+  canReadJobs: boolean;
+  canCreateJobs: boolean;
+  canUpdateJobs: boolean;
+  canDeleteJobs: boolean;
+  canReadJob: boolean;
+  canCreateJob: boolean;
+  canUpdateJob: boolean;
+  canDeleteJob: boolean;
+  canUpdateProfile: boolean;
+}
