a3863d20cc738eb30e4086c85995ccb328ceec82
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-13 11:09:02 +0200

    add a method to access the application trough the welcoming page

--- trova-job/src/app/components/unauthenticated-module/wrapper/welcome-page/welcome-page.component.ts
+++ trova-job/src/app/components/unauthenticated-module/wrapper/welcome-page/welcome-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { TrovaJobHelperService } from 'src/app/services/helper.service';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+  styleUrls: ['./welcome-page.component.scss'],
 })
 export class WelcomePageComponent implements OnInit {
+  cardTitle: string;
+  welcomeMessage: string;
 
-  constructor() { }
+  constructor(private helperService: TrovaJobHelperService) {}
 
   ngOnInit(): void {
+    this.cardTitle = 'Torino Trova Job';
+    this.welcomeMessage = "finding your job, it's our job";
   }
 
+  accessApplication() {
+    this.helperService.redirectTo('authentication/sign-in');
+  }
 }
