121f12310c70224010c01d63d0650c5179fc7106
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-13 15:31:07 +0200

    add class for check offerts template and scss file

---
+++ trova-job/src/app/components/authenticated-module/wrapper/check-offerts/check-offerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-offerts',
+  templateUrl: './check-offerts.component.html',
+  styleUrls: ['./check-offerts.component.scss']
+})
+export class CheckOffertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
