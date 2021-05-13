55c6a5b5ab50ef8fa3cf503d1cdf53576e7cd988
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-13 17:37:37 +0200

    import the shared module

--- trova-job/src/app/components/authenticated-module/wrapper/admin/admin.module.ts
+++ trova-job/src/app/components/authenticated-module/wrapper/admin/admin.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AdminRoutingModule } from './admin-routing.module';
 import { ManageUsersComponent } from './manage-users/manage-users.component';
 import { ManageJobsComponent } from './manage-jobs/manage-jobs.component';
 
+import { AdminRoutingModule } from './admin-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ManageUsersComponent, ManageJobsComponent],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  imports: [CommonModule, SharedModule, AdminRoutingModule],
 })
-export class AdminModule { }
+export class AdminModule {}
--- trova-job/src/app/components/authenticated-module/wrapper/comapny/comapny.module.ts
+++ trova-job/src/app/components/authenticated-module/wrapper/comapny/comapny.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ComapnyRoutingModule } from './comapny-routing.module';
 import { OfferJobComponent } from './offer-job/offer-job.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [OfferJobComponent],
-  imports: [
-    CommonModule,
-    ComapnyRoutingModule
-  ]
+  imports: [CommonModule, SharedModule, ComapnyRoutingModule],
 })
-export class ComapnyModule { }
+export class ComapnyModule {}
