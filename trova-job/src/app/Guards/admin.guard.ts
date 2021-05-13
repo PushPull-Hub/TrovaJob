344d870caa9cefed12e9d9b75ee4d5b5baf363ca
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-13 16:19:53 +0200

    generate a guard to restrict admin module access

---
+++ trova-job/src/app/Guards/admin.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
+import { TrovaJobHelperService } from '../services/helper.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivate {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private helperService: TrovaJobHelperService
+  ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authenticationService.getloggedInUser().then((user) => {
+      console.log('in');
+      if (user && user.role === 'admin') {
+        return true;
+      } else {
+        this.helperService.redirectTo('home');
+        return false;
+      }
+    });
+  }
+}
