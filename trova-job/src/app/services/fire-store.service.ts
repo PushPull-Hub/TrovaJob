660805ac03347687a7255b034381eee2cdd57e59
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-18 22:33:41 +0200

    remove the unused promises,imports and use retrieve configs strategy instead of abilities

--- trova-job/src/app/services/fire-store.service.ts
+++ trova-job/src/app/services/fire-store.service.ts
@@ -7,6 +7,9 @@ import { ErrorService, FireBaseErrors } from './error.service';
 import { User } from '../models/user.model';
 import { CustomErrorObject } from '../models/error.model';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+
+type Role = 'admin' | 'user' | 'company';
 @Injectable({ providedIn: 'root' })
 export class FireStoreCustomService {
   constructor(
@@ -19,11 +22,10 @@ export class FireStoreCustomService {
   async createUserOnFireStore(user: User, firebaseUserId: any): Promise<User> {
     return new Promise(async (resolve, reject) => {
       try {
-        const data =
-          await this.helperService.createUserObjectFromUserCustomClass(
-            user,
-            firebaseUserId
-          );
+        const data = this.helperService.createUserObjectFromUserCustomClass(
+          user,
+          firebaseUserId
+        );
         this.angularFirestore
           .doc(`users/${firebaseUserId}`)
           .set(data, { merge: true });
@@ -45,11 +47,7 @@ export class FireStoreCustomService {
             .subscribe(async (data) => {
               try {
                 const user =
-                  await this.helperService.convertFirebaseObjectToUserObject(
-                    data
-                  );
-                const abilities = await this.getUserAbilities(user.role);
-                user.abilities = abilities;
+                  this.helperService.convertFirebaseObjectToUserObject(data);
                 resolve(user);
               } catch (error) {
                 const customError = new CustomErrorObject(error.message, 401);
@@ -67,24 +65,17 @@ export class FireStoreCustomService {
     });
   }
 
-  getUserAbilities(userRole: string) {
-    return new Promise<any>((resolve, reject) => {
-      this.angularFirestore
-        .doc(`abilities/${userRole}`)
-        .valueChanges()
-        .subscribe(async (data) => {
-          try {
-            const abilities =
-              await this.helperService.convertFirebaseObjectToAbilitiesObject(
-                data
-              );
-            resolve(abilities);
-          } catch (error) {
-            const customError = new CustomErrorObject(error.message, 401);
-            this.errorService.errorOnSignIn.next(customError);
-            reject(null);
-          }
-        });
-    });
+  getConfigurations(userRole: Role): Observable<any> {
+    return this.angularFirestore
+      .doc(`configurations/${userRole}`)
+      .valueChanges();
   }
+
+  // development purposes
+  // storeOnFirebase(object: any, path: string) {
+  //   this.angularFirestore
+  //     .doc(path)
+  //     .set(object, { merge: true })
+  //     .then((res) => console.log(res, 'done'));
+  // }
 }
