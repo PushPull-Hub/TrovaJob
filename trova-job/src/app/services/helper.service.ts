581594f4d5f03b21a1891779bce6e7672803bfc3
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-18 22:36:14 +0200

    remove the unused promises and add more routes to the app

--- trova-job/src/app/services/helper.service.ts
+++ trova-job/src/app/services/helper.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
-import { UserAbilities } from '../models/abilities.model';
+import { Configuration } from '../models/configuration.model';
 
 import { User } from '../models/user.model';
 
 type applicationPaths =
-  | 'app/home'
+  | 'authentication/welcome-page'
   | 'authentication/sign-in'
   | 'authentication/sign-up'
-  | 'profile'
-  | 'authentication/welcome-page'
+  | 'app/home'
+  | 'app/profile'
+  | 'check-offerts'
+  | 'app/admin/manage-users'
+  | 'app/admin/manage-jobs'
+  | 'app/company/offer-job'
   | 'app/access-denied';
 
 @Injectable({
@@ -23,8 +26,8 @@ export class TrovaJobHelperService {
   createUserObjectFromUserCustomClass(
     user: User,
     firebaseUserId: any
-  ): Promise<any> {
-    return of({
+  ): { [key: string]: any } {
+    return {
       id: firebaseUserId,
       email: user.email,
       username: user.username,
@@ -32,13 +35,12 @@ export class TrovaJobHelperService {
       adress: user.adress,
       birthday: user.birthday,
       phoneNumber: user.phoneNumber,
-      abilities: {},
-    }).toPromise();
+    };
   }
 
   convertFirebaseObjectToUserObject(firebaseObject: {
     [key: string]: any;
-  }): Promise<User> {
+  }): User {
     let user = new User();
     firebaseObject
       ? ((user.adress = firebaseObject.adress ?? null),
@@ -48,32 +50,21 @@ export class TrovaJobHelperService {
         (user.phoneNumber = firebaseObject.phoneNumber ?? null),
         (user.role = firebaseObject.role ?? null),
         (user.id = firebaseObject.id ?? null),
-        (user.username = firebaseObject.username ?? null),
-        (user.abilities = firebaseObject.abilities ?? new UserAbilities()))
+        (user.username = firebaseObject.username ?? null))
       : (user = null);
-    return of(user).toPromise();
+    return user;
   }
 
-  convertFirebaseObjectToAbilitiesObject(firebaseObject: {
+  convertFirebaseObjectToConfigurationsObject(firebaseObject: {
     [key: string]: any;
-  }): Promise<UserAbilities> {
-    let abilities = new UserAbilities();
+  }) {
+    let configurations = new Configuration();
     firebaseObject
-      ? ((abilities.canReadUsers = firebaseObject.canReadUsers ?? false),
-        (abilities.canCreateUsers = firebaseObject.canCreateUsers ?? false),
-        (abilities.canUpdateUsers = firebaseObject.canUpdateUsers ?? false),
-        (abilities.canDeleteteUsers = firebaseObject.canDeleteteUsers ?? false),
-        (abilities.canReadJobs = firebaseObject.canReadJobs ?? false),
-        (abilities.canCreateJobs = firebaseObject.canCreateJobs ?? false),
-        (abilities.canUpdateJobs = firebaseObject.canUpdateJobs ?? false),
-        (abilities.canDeleteJobs = firebaseObject.canDeleteJobs ?? false),
-        (abilities.canReadJob = firebaseObject.canReadJob ?? false),
-        (abilities.canCreateJob = firebaseObject.canCreateJob ?? false),
-        (abilities.canUpdateJob = firebaseObject.canUpdateJob ?? false),
-        (abilities.canDeleteJob = firebaseObject.canDeleteJob ?? false),
-        (abilities.canUpdateProfile = firebaseObject.canUpdateProfile ?? false))
-      : (abilities = null);
-    return of(abilities).toPromise();
+      ? ((configurations.abilities = firebaseObject.abilities),
+        (configurations.cards = [...firebaseObject.cards]),
+        (configurations.navLinks = firebaseObject.navLinks))
+      : (configurations = null);
+    return configurations;
   }
 
   redirectTo(path: applicationPaths) {
