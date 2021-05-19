7a9c55527c651a7d090e4cfbee15701e1251991e
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-19 17:49:12 +0200

    refactor the shared module to fit the new use

--- trova-job/src/app/shared/shared.module.ts
+++ trova-job/src/app/shared/shared.module.ts
@@ -1,20 +1,50 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { CloudSvgComponent } from './cloud-svg/cloud-svg.component';
 import { AccessDeniedComponent } from './access-denied/access-denied.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+
+// Angular Material
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CheckOffertsComponent } from './check-offerts/check-offerts.component';
+import { NavComponent } from './nav/nav.component';
 
 const AngularMaterialModules = [
   MatCardModule,
   MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDatepickerModule,
   MatIconModule,
+  MatNativeDateModule,
   MatProgressSpinnerModule,
+  MatTooltipModule,
 ];
 @NgModule({
-  declarations: [CloudSvgComponent, AccessDeniedComponent],
-  imports: [...AngularMaterialModules],
-  exports: [CloudSvgComponent, AccessDeniedComponent],
+  declarations: [
+    CloudSvgComponent,
+    AccessDeniedComponent,
+    CheckOffertsComponent,
+    NavComponent,
+  ],
+  imports: [CommonModule, ...AngularMaterialModules, FontAwesomeModule],
+  exports: [
+    CloudSvgComponent,
+    AccessDeniedComponent,
+    CheckOffertsComponent,
+    NavComponent,
+  ],
 })
 export class SharedModule {}
