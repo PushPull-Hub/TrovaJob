ba5950ebdd5f548a43591f8e9b1487617f14cf6e
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-19 17:54:16 +0200

    use the new navbar dumb component

--- trova-job/src/app/components/unauthenticated-module/wrapper/wrapper.component.ts
+++ trova-job/src/app/components/unauthenticated-module/wrapper/wrapper.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BehaviorSubject, Subscription } from 'rxjs';
+import { ApplicationPossiblePaths } from 'src/app/models/app-paths.model';
+import { TrovaJobHelperService } from 'src/app/services/helper.service';
 import { AuthenticationService } from '../../../services/authentication.service';
 
 @Component({
@@ -10,12 +12,20 @@ import { AuthenticationService } from '../../../services/authentication.service'
 export class WrapperComponent implements OnInit, OnDestroy {
   loading: boolean;
   formLoadingSubscription: Subscription;
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private helperService: TrovaJobHelperService
+  ) {}
 
   ngOnInit(): void {
-    this.formLoadingSubscription = this.authenticationService.isFormLoading.subscribe(
-      (value: boolean) => (this.loading = value)
-    );
+    this.formLoadingSubscription =
+      this.authenticationService.isFormLoading.subscribe(
+        (value: boolean) => (this.loading = value)
+      );
+  }
+
+  redirectMeTo(event: ApplicationPossiblePaths) {
+    this.helperService.redirectTo(event);
   }
 
   ngOnDestroy(): void {
