9c8efdbc3d73cba19dc1c6bb441836fa52b2a6bf
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-20 16:30:17 +0200

    log the author name on the console

--- trova-job/src/app/app.component.ts
+++ trova-job/src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'trova-job';
+  constructor() {}
+  ngOnInit(): void {
+    console.log(
+      '  _    _                                   \n' +
+        ' | |  | |                                   \n' +
+        ' | |__| |   __ _   _ __ ___    ____   __ _  \n' +
+        " |  __  |  / _` | | '_ ` _ \\  |_  /  / _`  |\n" +
+        ' | |  | | | (_| | | | | | | |  / /  | (_| | \n' +
+        ' |_|  |_|  \\__,_| |_| |_| |_| /___|  \\__,_ |'
+    );
+  }
 }
