5e30dd268e89413793e3de78019870f042401c59
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-21 18:29:44 +0200

    load offerts up to the required city and render it into vertical cards

--- trova-job/src/app/components/job-offerts-module/wrapper/offerts-cards/offerts-cads.component.ts
+++ trova-job/src/app/components/job-offerts-module/wrapper/offerts-cards/offerts-cads.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApplicationPossiblePaths } from 'src/app/models/app-paths.model';
+import { JobOffert } from 'src/app/models/job.model';
+import { TrovaJobHelperService } from 'src/app/services/helper.service';
+import { JobsOffertsService } from 'src/app/services/jobs-offerts.service';
 
 @Component({
   selector: 'app-offert',
@@ -6,7 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offerts-cads.component.scss'],
 })
 export class OffertsCardsComponent implements OnInit {
-  constructor() {}
+  jobsOfferts: JobOffert[];
+  loading: boolean;
+  errorOnLoading: boolean;
+  constructor(
+    private jobsOffertsService: JobsOffertsService,
+    private activeRoute: ActivatedRoute,
+    private helperService: TrovaJobHelperService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loading = true;
+    this.activeRoute.params.subscribe((params) => {
+      const cityName = params['city'];
+      cityName
+        ? this.loadJobOffertsOf(cityName)
+        : this.helperService.redirectTo('job-offerts');
+    });
+  }
+
+  private loadJobOffertsOf(cityName) {
+    this.jobsOffertsService.getjobOffertsByCityName(cityName).then((data) => {
+      this.jobsOfferts = data;
+      setTimeout(() => {
+        this.errorOnLoading = false;
+        this.loading = false;
+      }, 300);
+    });
+  }
+
+  redirectMeTo(event: ApplicationPossiblePaths) {
+    this.helperService.redirectTo(event);
+  }
 }
