96e24b42de0dab4909638dc6e7db1c3737a456e6
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-21 12:06:47 +0200

    merge the 2 models since they complete each other

--- trova-job/src/app/models/configuration.model.ts
+++ trova-job/src/app/models/configuration.model.ts
@@ -1,10 +1,31 @@
-import { UserAbilities } from './abilities.model';
 import { Card } from './card.model';
 
 export class Configuration extends Object {
   abilities: UserAbilities;
-  cards: Card[];
+  cards: AppCards;
   navLinks: NavbarConfiguration | 'defaultConfiguration';
+}
+
+export class UserAbilities extends Object {
+  canReadUsers?: boolean;
+  canCreateUsers?: boolean;
+  canUpdateUsers?: boolean;
+  canDeleteteUsers?: boolean;
+  canReadJobs?: boolean;
+  canCreateJobs?: boolean;
+  canUpdateJobs?: boolean;
+  canDeleteJobs?: boolean;
+  canReadJob?: boolean;
+  canCreateJob?: boolean;
+  canUpdateJob?: boolean;
+  canDeleteJob?: boolean;
+  canUpdateProfile?: boolean;
+}
+
+export class AppCards extends Object {
+  home: Card[];
+  manageJobs?: Card[];
+  manageUsers?: Card[];
 }
 
 export class NavbarConfiguration extends Object {
