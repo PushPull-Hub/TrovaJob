a1fa73d5e9222c790d831c8d99782b983dac9c98
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-21 18:24:54 +0200

    add the initial main methods to load the job oferts

--- trova-job/src/app/services/jobs-offerts.service.ts
+++ trova-job/src/app/services/jobs-offerts.service.ts
@@ -2,59 +2,39 @@ import { Injectable } from '@angular/core';
 
 import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
 
-import { CompanyJobRequest, Job } from '../models/job.model';
-import { CityCard } from '../models/rtm-databse.model';
+import { JobOffert } from '../models/job.model';
+import { City, Jobs } from '../models/rtm-databse.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JobsOffertsService {
-  private dataBasePath: string = '/data';
   private availableCitiesPath: string = '/data/cities';
-  private cityPath: string = '/data/cities/city';
+  private jobsPath: string = '/data/jobs';
 
-  data: AngularFireObject<any>;
-  companiesJobsRequests: CompanyJobRequest[] = [];
-
-  availableCities: AngularFireObject<CityCard[]>;
+  availableCities: AngularFireObject<City[]>;
+  jobs: AngularFireObject<Jobs>;
 
   constructor(private angularFireDatabase: AngularFireDatabase) {
-    this.data = this.angularFireDatabase.object(this.dataBasePath);
     this.availableCities = this.angularFireDatabase.object(
       this.availableCitiesPath
     );
-  }
-
-  getTheDataBaseData(): Promise<Job[]> {
-    return new Promise((resolve, reject) => {
-      this.data.valueChanges().subscribe((data: Job[]) => {
-        data && data.length ? resolve(data) : resolve(null);
-      });
-    });
-  }
-
-  getOfferts(): Promise<CompanyJobRequest[]> {
-    return new Promise((resolve, reject) => {
-      this.getTheDataBaseData().then((data: Job[]) => {
-        if (data) {
-          let offerts: CompanyJobRequest[] = [];
-          data.map((job: Job) => {
-            offerts.push(job.request);
-          });
-          resolve(offerts);
-        } else {
-          reject(
-            'from UserService line 31 : the returned data array has no elements in it .'
-          );
-        }
-      });
-    });
+    this.jobs = this.angularFireDatabase.object(this.jobsPath);
   }
 
   getAvailableCities(): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.availableCities.valueChanges().subscribe((data: CityCard[]) => {
+      this.availableCities.valueChanges().subscribe((data: City[]) => {
         data ? resolve(data) : resolve(null);
+      });
+    });
+  }
+
+  getjobOffertsByCityName(cityName: string) {
+    return new Promise<any>((resolve, reject) => {
+      this.jobs.query.once('value').then((snapshot) => {
+        const jobs = snapshot.child(`${cityName}`).val();
+        resolve(Array.from(jobs.offerts));
       });
     });
   }
