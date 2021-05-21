86877baef72b005a321af9b56eab137c6976d557
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-21 18:25:55 +0200

    load available cities and render it as a clickable cards navigate to offerts

--- trova-job/src/app/components/job-offerts-module/wrapper/cities-cards/cities-card.component.ts
+++ trova-job/src/app/components/job-offerts-module/wrapper/cities-cards/cities-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CityCard } from 'src/app/models/rtm-databse.model';
+import { Router } from '@angular/router';
+import { City } from 'src/app/models/rtm-databse.model';
 import { JobsOffertsService } from 'src/app/services/jobs-offerts.service';
 
 @Component({
@@ -8,12 +9,16 @@ import { JobsOffertsService } from 'src/app/services/jobs-offerts.service';
   styleUrls: ['./cities-cards.component.scss'],
 })
 export class CitiesCardsComponent implements OnInit {
-  citiesCards: CityCard[];
-  constructor(private jobService: JobsOffertsService) {}
+  citiesCards: City[];
+  constructor(private jobService: JobsOffertsService, private router: Router) {}
 
   ngOnInit(): void {
     this.jobService.getAvailableCities().then((data) => {
       data ? (this.citiesCards = data) : null;
     });
+  }
+
+  takeMeToOffertsOf(cityName: string) {
+    this.router.navigateByUrl('job-offerts/' + cityName);
   }
 }
