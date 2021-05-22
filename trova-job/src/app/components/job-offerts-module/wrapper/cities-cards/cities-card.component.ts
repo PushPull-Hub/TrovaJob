e5377a179bde1d1667cf1dc0960a7b9bb1ad804c
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-22 23:31:27 +0200

    addd more logic to control whether is is required by admin or not

--- trova-job/src/app/components/job-offerts-module/wrapper/cities-cards/cities-card.component.ts
+++ trova-job/src/app/components/job-offerts-module/wrapper/cities-cards/cities-card.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Card } from 'src/app/models/card.model';
 import { City } from 'src/app/models/rtm-databse.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 import { JobsOffertsService } from 'src/app/services/jobs-offerts.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-cities-cards',
@@ -10,9 +13,29 @@ import { JobsOffertsService } from 'src/app/services/jobs-offerts.service';
 })
 export class CitiesCardsComponent implements OnInit {
   citiesCards: City[];
-  constructor(private jobService: JobsOffertsService, private router: Router) {}
+  adminAddNewCityCard: Card;
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private jobService: JobsOffertsService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    this.router.url == '/admin/job-offerts' ? this.checkifItsAdmin() : null;
+    this.loadCitiesCards();
+  }
+
+  private async checkifItsAdmin() {
+    const authUser = await this.authenticationService.getloggedInUser();
+    this.userService
+      .getSignleConfig(authUser.role, 'cards')
+      .then((cards: Card[]) => {
+        this.adminAddNewCityCard = cards['manageJobs'][0];
+      });
+  }
+
+  private loadCitiesCards() {
     this.jobService.getAvailableCities().then((data) => {
       data ? (this.citiesCards = data) : null;
     });
