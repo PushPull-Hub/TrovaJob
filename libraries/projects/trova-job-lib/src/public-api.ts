4062288595c749cf6ddc964d86354475856f2dd5
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-25 09:40:08 +0200

    export the lib module and remove the unexisting paths

--- libraries/projects/trova-job-lib/src/public-api.ts
+++ libraries/projects/trova-job-lib/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of trova-job-lib
  */
 
-export * from './lib/trova-job-lib.service';
-export * from './lib/trova-job-lib.component';
 export * from './lib/trova-job-lib.module';
