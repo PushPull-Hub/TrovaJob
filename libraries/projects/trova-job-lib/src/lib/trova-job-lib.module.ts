892ecad6805aaf6cf6c04d2e0438190a7ebf3c92
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-25 11:40:00 +0200

    add a forRoot method to the library and expext a config input from the user

--- libraries/projects/trova-job-lib/src/lib/trova-job-lib.module.ts
+++ libraries/projects/trova-job-lib/src/lib/trova-job-lib.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { CityCardComponent } from './components/city-card/city-card.component';
 import { ControlCardComponent } from './components/control-card/control-card.component';
 import { TestComponent } from './components/test/test.component';
+import { TrovaJobMainService } from './services/main.service';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,7 @@ import { TestComponent } from './components/test/test.component';
     ControlCardComponent,
     TestComponent,
   ],
-  imports: [],
+  imports: [CommonModule],
   exports: [
     NotFoundPageComponent,
     CityCardComponent,
@@ -19,4 +21,11 @@ import { TestComponent } from './components/test/test.component';
     TestComponent,
   ],
 })
-export class TrovaJobLibModule {}
+export class TrovaJobLibModule {
+  public static forRoot(config): ModuleWithProviders<TrovaJobLibModule> {
+    return {
+      ngModule: TrovaJobLibModule,
+      providers: [TrovaJobMainService, { provide: 'config', useValue: config }],
+    };
+  }
+}
