686915de4900e8a661b79fe50995c97b924edb19
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-25 17:25:36 +0200

    reset the logic of the directive and fix previous errors

--- trova-job/src/app/directives/create-Icon.directive.ts
+++ trova-job/src/app/directives/create-Icon.directive.ts
@@ -2,10 +2,10 @@ import {
   ComponentFactoryResolver,
   Directive,
   Input,
+  OnInit,
   ViewContainerRef,
 } from '@angular/core';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
+// icons
 import {
   faSearch,
   faUsersCog,
@@ -13,33 +13,14 @@ import {
   faBriefcase,
   faUserShield,
   faHeart,
+  faFolderPlus,
 } from '@fortawesome/free-solid-svg-icons';
-import { Card } from '../models/card.model';
+// types
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { IconConfigurations } from '../models/types/icon-configs.type';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
-@Directive({
-  selector: '[appCreatIcon]',
-})
-export class CreateIconDirective {
-  @Input('iconName') iconName: string;
-  constructor(
-    public viewContainerRef: ViewContainerRef,
-    private componentFactoryResolver: ComponentFactoryResolver
-  ) {}
-
-  createIcons(card: Card) {
-    const factory =
-      this.componentFactoryResolver.resolveComponentFactory(FaIconComponent);
-    const host = this.viewContainerRef;
-    host.clear();
-    const componentRef = host.createComponent(factory);
-    componentRef.instance.icon = importedIcons[card.icon];
-    componentRef.instance.size = '10x';
-    componentRef.instance.classes = ['fontawesome-icon'];
-    componentRef.instance.styles = { color: '#303f9f' };
-    componentRef.instance.render();
-  }
-}
-
+// icons Defenitions
 const importedIcons: { [key: string]: IconProp } = {
   searchIcon: faSearch,
   UsersSettingsIcon: faUsersCog,
@@ -47,4 +28,34 @@ const importedIcons: { [key: string]: IconProp } = {
   jobsSettingsIcon: faBriefcase,
   adminIcon: faUserShield,
   favIcon: faHeart,
+  addJobIcon: faFolderPlus,
 };
+
+@Directive({
+  selector: '[appCreatIcon]',
+})
+export class CreateIconDirective implements OnInit {
+  @Input('configurations') configurations: IconConfigurations;
+
+  constructor(
+    public viewContainerRef: ViewContainerRef,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
+
+  ngOnInit(): void {
+    this.configurations ? this.createIcon() : null;
+  }
+
+  createIcon(configurations: IconConfigurations = this.configurations) {
+    const factory =
+      this.componentFactoryResolver.resolveComponentFactory(FaIconComponent);
+    const host = this.viewContainerRef;
+    host.clear();
+    const componentRef = host.createComponent(factory);
+    componentRef.instance.icon = importedIcons[configurations.iconName];
+    componentRef.instance.size = configurations.size;
+    componentRef.instance.classes = configurations.classes;
+    componentRef.instance.styles = configurations.styles;
+    componentRef.instance.render();
+  }
+}
