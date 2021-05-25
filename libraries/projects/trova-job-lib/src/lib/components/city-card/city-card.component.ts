0833be076980316e448da957c650fc906b39a29d
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-25 09:35:47 +0200

    create the first component, style it and add the initial logic to it

--- libraries/projects/trova-job-lib/src/lib/components/city-card/city-card.component.ts
+++ libraries/projects/trova-job-lib/src/lib/components/city-card/city-card.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from '../../models/card.model';
 
 @Component({
-  selector: 'lib-city-card',
+  selector: 'tj-city-card',
   templateUrl: './city-card.component.html',
   styleUrls: ['./city-card.component.scss'],
 })
 export class CityCardComponent implements OnInit {
+  @Input() card: Card;
   constructor() {}
 
   ngOnInit(): void {}
+
+  takeMeToOffertsOf(path) {
+    console.log('emit to father');
+  }
 }
