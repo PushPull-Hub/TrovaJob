c7b88a06454b8e65c17624cad8f4689662a73823
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-27 17:21:13 +0200

    create a splited component to show while profile cover is on loading state

---
+++ trova-job/src/app/shared/profile/first-column/profile-cover-loading/profile-cover-loading.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-cover-loading',
+  templateUrl: './profile-cover-loading.component.html',
+  styleUrls: ['../profile-cover.component.scss'],
+})
+export class ProfileCoverLoadingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
