86bc67fe9bba2ac50536060a8b161f0d1b256b8f
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-27 17:24:15 +0200

    add mock up data and render profile components on the component father

--- trova-job/src/app/shared/profile/profile.component.ts
+++ trova-job/src/app/shared/profile/profile.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileConfiguration } from 'src/app/models/configuration.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  configurations: ProfileConfiguration;
+  constructor() {}
 
   ngOnInit(): void {
+    this.configurations = {
+      user: {
+        id: 'string',
+        email: 'string',
+        password: 'string',
+        username: 'string',
+        role: 'user',
+        adress: 'string',
+        birthday: {
+          day: 0,
+          month: 0,
+          year: 0,
+        },
+        phoneNumber: 3654654,
+      },
+      cover: {
+        user: {
+          id: '1',
+          username: 'John Scemo',
+          role: 'user',
+        },
+        coverImageUrl:
+          'https://images.unsplash.com/photo-1477132394330-d2376dc4c091?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
+        avatarImageUrl:
+          'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2578170.jpg',
+        buttons: [
+          {
+            type: 'user',
+            icon: 'UsersSettingsIcon',
+            onClickNavigateTo: '',
+          },
+          {
+            type: 'user',
+            icon: 'UsersSettingsIcon',
+            onClickNavigateTo: '',
+          },
+          {
+            type: 'user',
+            icon: 'UsersSettingsIcon',
+            onClickNavigateTo: '',
+          },
+        ],
+      },
+      body: null,
+    };
   }
-
 }
