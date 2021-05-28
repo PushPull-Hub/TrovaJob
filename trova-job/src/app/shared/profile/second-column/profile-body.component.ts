2dcae569c393fc886cdfc46c2a77b54a7233f19e
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-05-28 11:00:49 +0200

    render the profile body after the loading

--- trova-job/src/app/shared/profile/second-column/profile-body.component.ts
+++ trova-job/src/app/shared/profile/second-column/profile-body.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-profile-body',
   templateUrl: './profile-body.component.html',
-  styleUrls: ['./profile-body.component.scss']
+  styleUrls: ['./profile-body.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProfileBodyComponent implements OnInit {
+  asyncTabs: Observable<any[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.asyncTabs = new Observable((observer: Observer<any[]>) => {
+      setTimeout(() => {
+        observer.next([
+          { label: 'profile', content: 'profile-content' },
+          { label: 'offerts', content: 'offerts-content' },
+          { label: 'documents', content: 'documents-contents' },
+        ]);
+      }, 500);
+    });
   }
 
+  ngOnInit(): void {}
 }
